b3f40fbd35f3fd1022d2d820bf830d68b7c6ea09
NIRESH SHRESTHA <dewebkiller@gmail.com>
2024-03-10 21:38:40 +0545

    menu design and hover effect

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,47 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-function Navbar() {
+
+function Navbar(props) {
   const dewebkiller = "https://dewebkiller.com";
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <>
       <div className="main-header">
         <nav className="border-gray-200 dark:border-gray-700">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 float-right">
-            
-            <button data-collapse-toggle="navbar-default" type="button" class="iinline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-            
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded={isNavOpen ? "true" : "false"}
+              onClick={toggleNav}
+            >
+              <span className="sr-only">Toggle navigation</span>
+              {isNavOpen ? (
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              )}
+            </button>
+
             <div
-              className="hidden w-full md:block md:w-auto"
+              className={`${
+                isNavOpen ? "block mobilemenu" : "hidden"
+              } w-full md:block md:w-auto`}
               id="navbar-default"
             >
               <ul className="flex flex-col font-medium mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                 <Link to={"/"}>
-                  {" "}
                   <li>Home</li>
                 </Link>
                 <Link to={"/About"}>
-                  {" "}
                   <li>About</li>
                 </Link>
                 <Link to={"/Services"}>
-                  {" "}
                   <li>Services</li>
                 </Link>
                 <Link to={"/Portfolio"}>
-                  {" "}
                   <li>Portfolio</li>
                 </Link>
                 <Link to={dewebkiller} target="_blank">
-                  {" "}
                   <li>Blog</li>
                 </Link>
                 <Link to={"/Contact"}>
-                  {" "}
                   <li>Contact</li>
                 </Link>
               </ul>
@@ -52,4 +91,5 @@ function Navbar() {
     </>
   );
 }
+
 export default Navbar;
